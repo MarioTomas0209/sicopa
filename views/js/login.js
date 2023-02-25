97235609df69a5f47eb586d00ef93dd0164b69ba
Virbes <francisco.virbes.457@gmail.com>
2023-02-25 00:17:23 -0600

    marianos

--- views/js/login.js
+++ views/js/login.js
@@ -1,3 +1,13 @@
 // Variables declaration
-const 
+const correo = document.querySelector('#correo');
+const password = document.querySelector('#password');
+const login = document.querySelector('#login');
 // End of variables declaration
+
+login.addEventListener('click', login);
+
+
+function login(e) {
+    e.preventDefault();
+    alert('login');
+}
