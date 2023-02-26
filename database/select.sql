26c84eb6577754a9cc98aa5e916d8bcbec05c41c
Francisco Virbes <79809998+Virbes@users.noreply.github.com>
2023-02-26 03:59:08 -0600

    Update select.sql

--- database/select.sql
+++ database/select.sql
@@ -5,7 +5,3 @@ select * from paciente;
 select * from servicios;	
 select * from prefijo;
 select * from especialidad;
-
-INSERT INTO paciente(alergias, nombre, ape_pat, ape_mat, fec_nac, sexo, direccion, email, password) VALUES
-('', 'Francisco', 'Virbes', 'Juan', '', '', '', 'virbes@mail.com', 'virbes'),
-('', 'Mario Adolfo', 'Tomas', 'Roblero', '', '', '', 'mario@mail.com', 'mario');
