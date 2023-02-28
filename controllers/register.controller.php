e88d4b3f4303176da7950a6af4156358def462ce
mario tomas <tmario291@gmail.com>
2023-02-28 02:26:44 -0600

    iniciar sesion con registro

--- controllers/register.controller.php
+++ controllers/register.controller.php
@@ -18,12 +18,12 @@ class RegisterController{
 
             RegisterModel::mdlRegistration($nombre, $apellidos, $fec_nac, $email, $sexo, $direccion, $password);
 
-            // session_start();
-            $_SESSION[['paciente']] = 'paciente';
-            $_SESSION[['id']] = 1;
-            $_SESSION[['nombre']] = 'nombre';
-            $_SESSION[['apellidos']] = 'apellidos';
-            $_SESSION[['email']] = 'email';
+            session_start();
+            $_SESSION['usuario'] = 'paciente';
+            $_SESSION['id'] = 1;
+            $_SESSION['nombre'] = $nombre;
+            $_SESSION['apellidos'] = $apellidos;
+            $_SESSION['email'] = $email;
 
             header('location: main');
     
